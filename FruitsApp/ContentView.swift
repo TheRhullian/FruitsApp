2300a04c06b8c86d75c665b51525852f88b362a5
Rhullian Damião <rhullian4545@gmail.com>
2022-03-06 19:08:03 -0300

    Initial Commit

---
+++ FruitsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FruitsApp
+//
+//  Created by Rhullian Damião on 06/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FruitsApp/FruitsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsAppApp.swift
+//  FruitsApp
+//
+//  Created by Rhullian Damião on 06/03/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
